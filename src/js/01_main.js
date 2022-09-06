402fffd874a2674a0f38fe835685feaabd117560
Denis Tyukavin <dtyukavin@dmvdesk.com>
2022-09-07 01:34:27 +0300

    animation fixed

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -2,17 +2,40 @@
 
   document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("load");
+    const wow = new WOW({
+      boxClass: 'wow',
+      animateClass: 'animate__animated',
+      offset: 0,
+      mobile: false,
+    });
 
-    const wow = new WOW(
-      {
-        boxClass: 'wow',
-        animateClass: 'animate__animated',
-        offset: 0,
-        mobile: false,
-      }
-    );
     wow.init();
+
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({triggerElement: ".provider-slider"})
+      .setClassToggle(".provider-slider", "active")
+      .addTo(controller);
+    
+    new ScrollMagic.Scene({triggerElement: ".leave-brand-slider"})
+      .setClassToggle(".leave-brand-slider", "active")
+      .addTo(controller);
+    
+    new ScrollMagic.Scene({triggerElement: ".platform-lines"})
+      .setClassToggle(".js-anim-1", "anim-1")
+      .addTo(controller);
+    
+    new ScrollMagic.Scene({triggerElement: ".platform-lines"})
+      .setClassToggle(".js-anim-2", "anim-2")
+      .addTo(controller);
+    
+    new ScrollMagic.Scene({triggerElement: ".platform-lines"})
+      .setClassToggle(".js-anim-3", "anim-3")
+      .addTo(controller);
+    
+    new ScrollMagic.Scene({triggerElement: ".platform-lines"})
+      .setClassToggle(".js-anim-4", "anim-4")
+      .addTo(controller);
 
     const sceneSafe = document.querySelector(".safe-content");
     const parallaxInstance1 = new Parallax(sceneSafe);
@@ -22,8 +45,21 @@
 
     const myModal = new HystModal({
       linkAttributeName: "data-hystmodal",
-      //settings (optional). see API
     });
+
+    const btnMore = document.querySelector(".js-btn-more");
+    const rowVisible = document.querySelector(".js-row-visible");
+    const imgAdded = document.querySelectorAll(".js-store-add");
+
+    btnMore.addEventListener("click", function() {
+
+      this.setAttribute("disabled", "disabled");
+      rowVisible.classList.toggle("store-content__row--hidden");
+      imgAdded.forEach((elem) => {
+        elem.className = "animate__animated animate__zoomIn wow";
+      });
+    });
+
   });
 
 })();
